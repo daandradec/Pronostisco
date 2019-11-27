c56b86ab8afe3b6f62254f6d0e5c27b07b57fad4
daandradec <daandradec@unal.edu.co>
2019-11-27 18:15:42 -0500

    download tree

--- core/static/core/vendor/d3-tree/D3Tree.js
+++ core/static/core/vendor/d3-tree/D3Tree.js
@@ -32,6 +32,28 @@ function startD3Tree(){
     zoomListener.scale(3);
 
     D3Tree(tree_mrp);
+
+    document.querySelector("button[tree-download='true']").addEventListener("click",function(e){
+        e.stopPropagation();
+        const svg = e.target.parentElement.parentElement.children[1].children[0].children[0];
+        const canvas = document.createElement('canvas');
+        canvas.width = viewerWidth;
+        canvas.height = viewerHeight;
+        var data = (new XMLSerializer()).serializeToString(svg);    
+        /* We can just create a canvas element inline so you don't even need one on the DOM. Cool!
+        canvg(canvas, data, {
+            renderCallback: function() {
+              canvas.toBlob(function(blob) {
+                  download('MyImageName.png', blob);
+              });
+            }
+          });  */ 
+          
+        // usage:
+        drawInlineSVG(canvas.getContext("2d"), data, function() {
+            downloadLink("Archivo.png", canvas.toDataURL());
+        });
+    }, false);
 }
 
 function D3Tree(treeData){
@@ -54,3 +76,47 @@ function D3Tree(treeData){
 
 
 //window.addEventListener("load", startD3Tree, false);
+
+
+//  VERSION 1 CON CANVG
+function download(filename, /* string */ blob /* Blob*/) {
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, filename);
+    } else {
+      const elem = window.document.createElement('a');
+      elem.href = window.URL.createObjectURL(blob);
+      elem.download = filename;
+      document.body.appendChild(elem);
+      elem.click();
+      document.body.removeChild(elem);
+    }
+}
+
+// funcion para generar el download link, mismo codigo del else del de arriba
+function downloadLink(filename, url){
+    const elem = window.document.createElement('a');
+    elem.href = url;
+    elem.download = filename;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+}
+  
+
+
+  // VERSION 2 SIN CANVG
+function drawInlineSVG(ctx, rawSVG, callback) {
+
+    var svg = new Blob([rawSVG], {type:"image/svg+xml;charset=utf-8"}),
+        domURL = self.URL || self.webkitURL || self,
+        url = domURL.createObjectURL(svg),
+        img = new Image;
+
+    img.onload = function () {
+        ctx.drawImage(this, 0, 0);     
+        domURL.revokeObjectURL(url);
+        callback(this);
+    };
+
+    img.src = url;
+}
